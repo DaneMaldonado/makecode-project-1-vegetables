e56ec6aeb03ec5556991d737bb5e6afe736ad09d
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2025-02-05 22:57:20 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,320 @@
+namespace SpriteKind {
+    export const Location = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    characterAnimations.loopFrames(
+    VegHero,
+    [img`
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f e e e e e e e f f . 
+        f f e f e e e e e e f f 
+        f f f e e e e e e e e f 
+        f f f e e e e e e f e f 
+        f f f f e e e e f f f f 
+        f f f f f f f f f f f f 
+        f f f f f f f f f f f f 
+        . f f f f f f f f f f . 
+        . e f f f f f f f f e . 
+        e 4 f b b b b b b f 4 e 
+        4 d f d d d d d d c d 4 
+        4 4 f 6 6 6 6 6 6 f 4 4 
+        . . . f f f f f f . . . 
+        . . . f f . . f f . . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f e e e e e e e f f . 
+        f e e f e e e e e e f f 
+        f f f e e e e e e e e f 
+        f f e e e e e e e f e f 
+        f f f e e e e f f f f f 
+        f f f f f f f f f f f f 
+        f f f f f f f f f f f f 
+        . f f f f f f f f f f . 
+        . e f f f f f f f f e . 
+        . 4 f b b b b b f e 4 e 
+        . 4 f d d d d d e d d 4 
+        . e f f f f f f e e 4 . 
+        . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f e e e e e e e f f . 
+        f f e f e e e e e e f f 
+        f f f e e e e e e e e f 
+        f f f f e e e e e f e f 
+        f f f f f e e e e f f f 
+        f f f f f f f f f f f f 
+        f f f f f f f f f f f f 
+        . f f f f f f f f f f . 
+        . e f f f f f f f f e . 
+        e 4 e f b b b b b f 4 . 
+        4 d d e d d d d d f 4 . 
+        . 4 e e f f f f f f e . 
+        . . . . . . . f f f . . 
+        `],
+    100,
+    characterAnimations.rule(Predicate.MovingUp)
+    )
+    scene.cameraFollowSprite(VegHero)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile13`, function (sprite, location) {
+    VegChangeCameraPers()
+})
+function SpawnVegHero () {
+    VegHero = sprites.create(img`
+        . . . . f f f f . . . . . 
+        . . f f c c c c f f . . . 
+        . f f c c c c c c f f . . 
+        f f c c c c c c c c f f . 
+        f f c c f c c c c c c f . 
+        f f f f f c c c f c c f . 
+        f f f f c c c f c c f f . 
+        f f f f f f f f f f f f . 
+        f f f f f f f f f f f f . 
+        . f f f f f f f f f f . . 
+        . f f f f f f f f f f . . 
+        f e f f f f f f f f e f . 
+        e 4 f 7 7 7 7 7 7 c 4 e . 
+        e e f 6 6 6 6 6 6 f e e . 
+        . . . f f f f f f . . . . 
+        . . . f f . . f f . . . . 
+        `, SpriteKind.Player)
+    tiles.placeOnTile(VegHero, tiles.getTileLocation(5, 17))
+    scene.cameraFollowSprite(VegHero)
+    controller.moveSprite(VegHero)
+}
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    characterAnimations.loopFrames(
+    VegHero,
+    [img`
+        . . . . . f f f f f . . . 
+        . . . f f f f f f f f f . 
+        . . f f f c f f f f f f . 
+        . . f f c f f f c f f f f 
+        f f c c f f f c c f f c f 
+        f f f f f e f f f f c c f 
+        . f f f e e f f f f f f f 
+        . . f f e e f b f e e f f 
+        . . . f 4 4 f 1 e 4 e f . 
+        . . . f 4 4 4 4 e f f f . 
+        . . . f f e e e e e f . . 
+        . . . f 7 7 7 e 4 4 e . . 
+        . . . f 7 7 7 e 4 4 e . . 
+        . . . f 6 6 6 f e e f . . 
+        . . . . f f f f f f . . . 
+        . . . . . . f f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . 
+        . . . f f f f f f f f f . 
+        . . f f f c f f f f f f . 
+        . f f f c f f f c f f f f 
+        f f c c f f f c c f f c f 
+        f f f f f e f f f f c c f 
+        . f f f e e f f f f f f f 
+        . . f f e e f b f e e f f 
+        . . f f 4 4 f 1 e 4 e f . 
+        . . . f 4 4 4 e e f f f . 
+        . . . f f e e 4 4 e f . . 
+        . . . f 7 7 e 4 4 e f . . 
+        . . f f 6 6 f e e f f f . 
+        . . f f f f f f f f f f . 
+        . . . f f f . . . f f . . 
+        `,img`
+        . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . 
+        . . . f f f f f f f f f . 
+        . . f f f c f f f f f f . 
+        . f f f c f f f c f f f f 
+        f f c c f f f c c f f c f 
+        f f f f f e f f f f c c f 
+        . f f f e e f f f f f f f 
+        . f f f e e f b f e e f f 
+        . . f f 4 4 f 1 e 4 e f f 
+        . . . f 4 4 4 4 e f f f . 
+        . . . f f e e e e 4 4 4 . 
+        . . . f 7 7 7 7 e 4 4 e . 
+        . . f f 6 6 6 6 f e e f . 
+        . . f f f f f f f f f f . 
+        . . . f f f . . . f f . . 
+        `],
+    100,
+    characterAnimations.rule(Predicate.MovingLeft)
+    )
+    scene.cameraFollowSprite(VegHero)
+})
+function VegChangeCameraPers () {
+    VegChangeCamera = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . d . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food)
+    tiles.placeOnTile(VegChangeCamera, tiles.getTileLocation(6, 2))
+    scene.cameraFollowSprite(VegChangeCamera)
+}
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    characterAnimations.loopFrames(
+    VegHero,
+    [img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e e f . . 
+        f f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f . 
+        f f 4 d 4 d d d d f . . 
+        . f f f 4 d d b b f . . 
+        . 4 d d e 4 4 4 e f . . 
+        . e d d e 1 1 1 1 f . . 
+        . f e e f 6 6 6 6 f f . 
+        . f f f f f f f f f f . 
+        . . f f . . . f f f . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e e f . . 
+        f f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f . 
+        . f 4 d 4 d d d d f . . 
+        . f f f e e d b b f . . 
+        . . f 4 d d e 4 e f . . 
+        . . f e d d e 1 1 f . . 
+        . f f f e e f 6 6 f f . 
+        . f f f f f f f f f f . 
+        . . f f . . . f f f . . 
+        `,img`
+        . . . . f f f f f . . . 
+        . . f f e e e e e f . . 
+        . f f e e e e e e e f . 
+        f f f f e e e e e e e f 
+        f f f f f e e e 4 e e f 
+        f f f f e e e 4 4 e e f 
+        f f f f 4 4 4 4 4 e f f 
+        f f 4 e 4 f f 4 4 e f f 
+        . f 4 d 4 d d d d f f . 
+        . f f f 4 d d b b f . . 
+        . . f e e 4 4 4 e f . . 
+        . . 4 d d e 1 1 1 f . . 
+        . . e d d e 1 1 1 f . . 
+        . . f e e f 6 6 6 f . . 
+        . . . f f f f f f . . . 
+        . . . . f f f . . . . . 
+        `],
+    100,
+    characterAnimations.rule(Predicate.MovingRight)
+    )
+    scene.cameraFollowSprite(VegHero)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    characterAnimations.loopFrames(
+    VegHero,
+    [img`
+        . . . . f f f f . . . . 
+        . . f f e e e e f f . . 
+        . f f e e e e e e f f . 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        . f e d d b b d d e f . 
+        . f f e 4 4 4 4 e f f . 
+        e 4 f b 1 1 1 1 b f 4 e 
+        4 d f 1 1 1 1 1 1 f d 4 
+        4 4 f 6 6 6 6 6 6 f 4 4 
+        . . . f f f f f f . . . 
+        . . . f f . . f f . . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e f f f . 
+        f f f e e e e e e f f f 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        . f e d d b b d 4 e f e 
+        f f f e 4 4 4 4 d d 4 e 
+        e 4 f b 1 1 1 e d d e . 
+        . . f 6 6 6 6 f e e . . 
+        . . f f f f f f f . . . 
+        . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . 
+        . . . f f f f f f . . . 
+        . f f f e e e e f f f . 
+        f f f e e e e e e f f f 
+        f f f f 4 e e e f f f f 
+        f f f 4 4 4 e e f f f f 
+        f f f 4 4 4 4 e e f f f 
+        f 4 e 4 4 4 4 4 4 e 4 f 
+        f 4 4 f f 4 4 f f 4 4 f 
+        f e 4 d d d d d d 4 e f 
+        e f e 4 d b b d d e f . 
+        e 4 d d 4 4 4 4 e f f f 
+        . e d d e 1 1 1 b f 4 e 
+        . . e e f 6 6 6 6 f . . 
+        . . . f f f f f f f . . 
+        . . . . . . . f f f . . 
+        `],
+    500,
+    characterAnimations.rule(Predicate.MovingDown)
+    )
+    scene.cameraFollowSprite(VegHero)
+})
+function titlereplacement () {
+    for (let value of tiles.getTilesByType(assets.tile`VegSpawnHeroTile`)) {
+        VegRocks = sprites.create(img`
+            . . b d b . . . . . b b b b . . 
+            . c b d d b . . . b b d d d b . 
+            . b c c b . . . b c d d d d b . 
+            . . . . . . b b c c b d b b b . 
+            . . . . . b d d b c c b b b c . 
+            . . b b b c d d b b c c c c . . 
+            . b d d d b c b b c . . . . . . 
+            c b d d d d c c c c . b b b . . 
+            c c b b b b c c c . b d d d b . 
+            . c c c b b . . b c b b d d b b 
+            . b b . . . . . b c c b b b b . 
+            b d d b b . . . . . c c c b . . 
+            b b d d b c . . b b b b b b b . 
+            . b c c c b . b d d d b b c b . 
+            . . . . . . b d d d b c c b . . 
+            . . . . . . b b b c c c b . . . 
+            `, SpriteKind.Location)
+        tiles.placeOnTile(VegRocks, value)
+    }
+}
+let VegRocks: Sprite = null
+let VegChangeCamera: Sprite = null
+let VegHero: Sprite = null
+tiles.setCurrentTilemap(tilemap`level2`)
+titlereplacement()
+SpawnVegHero()
